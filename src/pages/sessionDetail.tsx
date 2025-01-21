334ef8a8d1ec510827fcc2b761366ce65945c8e1
EvanSmith93 <evan.smith993@gmail.com>
2025-01-21 00:19:11 -0700

    Fix end session link

--- src/pages/sessionDetail.tsx
+++ src/pages/sessionDetail.tsx
@@ -1,4 +1,6 @@
+import { Button } from "react-bootstrap";
 import summaryBar from "../assets/summaryBar.png";
+import { Link } from "react-router-dom";
 
 export default function SessionDetail() {
   return (
@@ -10,7 +12,6 @@ export default function SessionDetail() {
           RTUJV
         </div>
 
-        {/* This is where the data from the third party API call will go (from zenquotes.io) */}
         <div id="notification" className="alert alert-warning">
           You've been on break for 15 minutes. Ready to get back into it?
           <br />
@@ -32,16 +33,11 @@ export default function SessionDetail() {
           height="45px"
         />
 
-        {/* A WebSocket connection will go here, so other joined users can see when you start/finish a break or end the session */}
         <div>
-          <button className="btn btn-primary" id="start-stop-break">
-            Finish Break
-          </button>
-          <a href="list.html">
-            <button className="btn btn-primary" id="end-session">
-              End Session
-            </button>
-          </a>
+          <Button id="start-stop-break">Finish Break</Button>
+          <Link to="/dashboard">
+            <Button id="end-session">End Session</Button>
+          </Link>
         </div>
       </div>
     </>
