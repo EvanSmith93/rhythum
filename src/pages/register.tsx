c2d9aab52ba237aaca2339c2d296d1757e6f50c4
EvanSmith93 <evan.smith993@gmail.com>
2025-01-20 19:45:11 -0700

    Add register page and start to import styles

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,11 +1,14 @@
-import '../styles/main.css';
+import "../styles/main.css";
 import "../styles/form.css";
 
-export default function Login() {
+export default function Register() {
   return (
     <>
-      <h3>Log In</h3>
+      <h3>Sign Up</h3>
       <form method="get" action="list.html" className="form-group">
+        <div>
+          <input type="text" placeholder="name" className="form-control" />
+        </div>
         <div>
           <input type="email" placeholder="email" className="form-control" />
         </div>
@@ -18,10 +21,10 @@ export default function Login() {
         </div>
         <div>
           <button type="submit" className="btn btn-primary">
-            Log In
+            Sign Up
           </button>
         </div>
-        <a href="register.html">New? Sign up.</a>
+        <a href="index.html">Have an account? Log in.</a>
       </form>
     </>
   );
