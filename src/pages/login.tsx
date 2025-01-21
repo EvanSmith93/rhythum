3aeb2493c0dc48b356c9428470cd5b0d8b6e29cc
EvanSmith93 <evan.smith993@gmail.com>
2025-01-20 20:34:33 -0700

    Add React Bootstrap

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,28 +1,17 @@
-import '../styles/main.css';
+import "../styles/main.css";
 import "../styles/form.css";
+import { Button, Form } from "react-bootstrap";
 
 export default function Login() {
   return (
     <>
       <h3>Log In</h3>
-      <form method="get" action="list.html" className="form-group">
-        <div>
-          <input type="email" placeholder="email" className="form-control" />
-        </div>
-        <div>
-          <input
-            type="password"
-            placeholder="password"
-            className="form-control"
-          />
-        </div>
-        <div>
-          <button type="submit" className="btn btn-primary">
-            Log In
-          </button>
-        </div>
+      <Form method="get" action="list.html">
+        <Form.Control type="email" placeholder="email" />
+        <Form.Control type="password" placeholder="password" />
+        <Button type="submit">Log In</Button>
         <a href="register.html">New? Sign up.</a>
-      </form>
+      </Form>
     </>
   );
 }
