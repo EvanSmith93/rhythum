b6fae398d93f5c71ed59774478a6d3da17157cfa
EvanSmith93 <evan.smith993@gmail.com>
2025-01-20 21:03:53 -0700

    Add footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "./header";
 import "../styles/main.css";
+import Footer from "./footer";
 
 export default function Layout() {
   return (
-    <>
+    <body>
       <Header />
       <main>
         <Outlet />
       </main>
-    </>
+      <Footer />
+    </body>
   );
 }
