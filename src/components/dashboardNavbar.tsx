ca26f79c22fdfa5b0092140afaa8ba19eafea095
EvanSmith93 <evan.smith993@gmail.com>
2025-01-22 00:30:48 -0700

    Use react router for the navigation in the navbar

--- src/components/dashboardNavbar.tsx
+++ src/components/dashboardNavbar.tsx
@@ -1,11 +1,18 @@
 import { Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function DashboardNavbar() {
   return (
     <Nav className="ms-auto me-4">
-      <Nav.Link href="/sessions/0">New Session</Nav.Link>
-      <Nav.Link href="/join-session">Join with Session Code</Nav.Link>
-      <Nav.Link href="/">Log Out</Nav.Link>
+      <Nav.Link as={Link} to="/sessions/0">
+        New Session
+      </Nav.Link>
+      <Nav.Link as={Link} to="/join-session">
+        Join with Session Code
+      </Nav.Link>
+      <Nav.Link as={Link} to="/">
+        Log Out
+      </Nav.Link>
     </Nav>
   );
 }
