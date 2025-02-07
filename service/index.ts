ff10f9e4932ee71d7f0748dc4dafb6294fdd543c
EvanSmith93 <evan.smith993@gmail.com>
2025-02-06 22:11:01 -0700

    Add register route

--- service/index.ts
+++ service/index.ts
@@ -1,9 +1,16 @@
+import { hash } from "bcrypt";
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.post("/api/auth", async (req, res) => {
-  res.send({ email: "marta@id.com" });
+  if (await getUser("email", req.body.email)) {
+    res.status(409).send({ msg: "Existing user" });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+    res.send({ email: user.email });
+  }
 });
 
 app.put("/api/auth", async (req, res) => {
@@ -22,3 +29,27 @@ const port = 3000;
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
+const users: User[] = [];
+
+async function createUser(email: string, password: string) {
+  const passwordHash = await hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+  };
+
+  users.push(user);
+
+  return user;
+}
+
+async function getUser(field: keyof User, value: string) {
+  return users.find((user) => user[field] === value);
+}
+
+type User = {
+  email: string;
+  password: string;
+};
