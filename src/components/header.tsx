777a8d8287d58386aa8111464d797a7b8656079c
EvanSmith93 <evan.smith993@gmail.com>
2025-04-04 20:11:01 -0600

    prevent unneeded page rerender when going to the dashboard

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,15 @@
 import { Navbar } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Header({ navbar }: { navbar?: ReactNode }) {
+  const navigate = useNavigate();
+
   return (
     <header>
       <Navbar className="p-0 ms-4">
-        <Navbar.Brand className="navbar-brand" href="/">
+        <Navbar.Brand className="navbar-brand" onClick={() => navigate("/")}>
           <img src={logo} width="46px" alt="Rhythum Logo" />
           Rhythum
         </Navbar.Brand>
